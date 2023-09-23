3779ab01e4e0409548ab575424a4694ef59e1afb
NithanthSujith <76246605+NithanthSujith@users.noreply.github.com>
2023-09-23 13:32:50 +0530

    Class based component structure

---
+++ src/Components/NewsItem.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+
+  render() {
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+    return (
+      <div>
+        <div className="card">
+        <div style={{ display: "flex", justifyContent:'flex-end', position:'absolute', right:'0' }}>
+              <span className=" badge rounded-pill bg-danger" >
+                {source}</span>
+            </div>
+          <img src={!imageUrl ? 'https://economictimes.indiatimes.com/thumb/msid-103059769,width-1070,height-580,imgsize-268482,overlay-etmarkets/photo.jpg' : imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-body-secondary">By {!author ? "Unkown" : author} on {new Date(date).toUTCString()}</small></p>
+            <a rel='noreferrer' href={newsUrl} target='_blank' className="btn btn-primary">Read..</a>
+          </div>
+
+        </div>
+
+      </div>
+
+    )
+
+  }
+}
+
+export default NewsItem
---
+++ src/Components/Spinner.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import book from './Book.gif'
+export class Spinner extends Component {
+  render() {
+    return (
+      <div className='text-center'>
+        <img className='my-3' src={book} alt="" />
+      </div>
+    )
+  }
+}
+
+export default Spinner
